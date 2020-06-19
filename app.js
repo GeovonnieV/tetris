101f0341b00cc7c60e4c5e95511d581cd9e476bd
geovonnie vasquez <geovonnievasquez@gmail.com>
2020-06-18 19:52:03 -0500

    base of page started, grabed all the elements in js

---
+++ app.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DomContentLoaded', () => {
+
+        const grid = document.querySelector(".grid")
+        // the array from lets us get all the divs in the grid and turn them into an array we can work with
+        // now every div has an index
+        let squares = Array.from(document.querySelectorAll(".grid div"))
+        const scoreDisplay = document.querySelector("#score")
+        const startBtn = document.querySelector("#start-button")
+        const width = 10
+
+        
+
+// })
